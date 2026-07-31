c4fda1e5011d67cdb6a175c58542ce01f2069084
Hafsa19work <hafsaaqeel18@gmail.com>
2026-07-31 22:12:59 +0500

    Update response message to version 3

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, Response
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Jenkins CI/CD pipeline works!\n This is my v2"
+    return Response("Jenkins CI/CD pipeline works!\n This is my v3", mimetype="text/plain")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
